f68d7e11216090c11bbc297327cf2fc3bab82fe1
paco <franciscosprouse@gmail.com>
2021-01-22 23:19:03 -0800

    simplified settngs.py since this is just a dev env for now

--- pacodjango/pacodjango/settings.py
+++ pacodjango/pacodjango/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'jnlxo(j49r@42pe9llt+bsv1&4-3q_j8))a)5^pdq@&tqm%&^j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.0.37", "localhost", "127.0.0.1"]
+# NOT PRODUCTION READY!!!
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
