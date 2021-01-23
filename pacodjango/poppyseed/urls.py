6b0c6ecdb593908bdda0dabce88daf43bc370c11
paco <franciscosprouse@gmail.com>
2021-01-22 22:10:10 -0800

    added MCP3008 to read the digital output from the water sensor

--- pacodjango/poppyseed/urls.py
+++ pacodjango/poppyseed/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('hydration/', views.hydration, name='hydration'),
 ]
--- pacodjango/poppyseed/views.py
+++ pacodjango/poppyseed/views.py
@@ -1,8 +1,19 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from poppyseed.MCP3008 import MCP3008
 
-# Create your views here.
-from django.http import HttpResponse
-
+DRY = 760
+MAX = 1023
+MIN = 0
+OFF = 11
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def hydration(request):
+    adc = MCP3008()
+    value = adc.read( channel = 0 ) # You can of course adapt the channel to be read out
+    return JsonResponse({'sensor 1': value})
+
+
+
